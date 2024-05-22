1dc7005825731abd843d05c99b69139131a0903f
victor@victor <victor.dmaina@gmail.com>
2024-05-22 16:36:14 +0300

    list comprehension pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -3,5 +3,12 @@
 def return_evens(num_list):
     pass
 
+def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
+
 def make_exclamation(sentence_list):
     pass
+
+def make_exclamation(sentence_list):
+    return [sentence + "!" for sentence in sentence_list]
+    
